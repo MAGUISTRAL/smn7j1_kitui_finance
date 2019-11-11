5f504d4aa84d10ca1cc6d70d9da244c66080a7da
Cédric RENAUD <ruthhaba@gmail.com>
2019-11-11 16:52:13 +0100

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'statique/index'
-  root 'welcome#index' 
+  root 'statique#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
